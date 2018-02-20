129335514573301aa23e861e5b206158d9c4742e
shivapbhusal <civabhusal@gmail.com>
2018-02-20 16:34:31 -0500

    finalize generateDataset

--- python/generateData.py
+++ python/generateData.py
@@ -8,47 +8,40 @@ from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import numpy as np
 
+def generateDatset(x,y,z):
+    dataList=[]
+    i=0.5
+    while (i<=2):
+        dataList.append([x+i,y,z])
+        dataList.append([x-i,y,z])
+        i=i+0.5
+
+    j=0.5
+    while (j<=2):
+        dataList.append([x,y+j,z])
+        dataList.append([x,y-j,z])
+        j=j+0.5
+
+    k=0.5
+    while (k<=2):
+        dataList.append([x,y,z+k])
+        dataList.append([x,y,z-k])
+        k=k+0.5
+    
+    return dataList
+
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
 centers=[[0,0,0],[5,5,5],[-5,-5,-5]]
 
-
 for x,y,z in centers:
     ax.scatter(x,y,z,c='r')
 
-ax.scatter(0,0,0.5,c='b')
-ax.scatter(0,0,1,c='b')
-ax.scatter(0,0,1.5,c='b')
-ax.scatter(0,0,2,c='b')
-
-ax.scatter(0.5,0,0,c='b')
-ax.scatter(1,0,0,c='b')
-ax.scatter(1.5,0,0,c='b')
-ax.scatter(2,0,0,c='b')
-
-ax.scatter(0,0.5,0,c='b')
-ax.scatter(0,1,c='b')
-ax.scatter(0,1.5,0,c='b')
-ax.scatter(0,2,0,c='b')
-
-ax.scatter(0,0,-0.5,c='b')
-ax.scatter(0,0,-1,c='b')
-ax.scatter(0,0,-1.5,c='b')
-ax.scatter(0,0,-2,c='b')
-
-ax.scatter(-0.5,0,0,c='b')
-ax.scatter(-1,0,0,c='b')
-ax.scatter(-1.5,0,0,c='b')
-ax.scatter(-2,0,0,c='b')
-
-ax.scatter(0,-0.5,0,c='b')
-ax.scatter(0,-1,c='b')
-ax.scatter(0,-1.5,0,c='b')
-ax.scatter(0,-2,0,c='b')
-
-
-
+for x,y,z in [[0,0,0],[-5,-5,-5],[5,5,5]]:
+    dataList=generateDatset(x,y,z)
+    for x_o,y_o,z_o in dataList:
+         ax.scatter(x_o,y_o,z_o,c='b')
 
 ax.set_xlabel('X Label')
 ax.set_ylabel('Y Label')
