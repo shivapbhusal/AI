602241d93088f5d5eb48ccade81adbb7b9d35e71
shivapbhusal <civabhusal@gmail.com>
2018-02-19 22:16:13 -0500

    change naming convention

--- python/perceptron.py
+++ python/perceptron.py
@@ -1,3 +1,8 @@
+'''
+Three class Perceptron implementation 
+Author: Shiva Bhusal, BGSU 
+
+'''
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import numpy as np
@@ -23,17 +28,21 @@ ax.set_zlabel('Z Label')
 
 plt.show()
 
-def calculateOutput(theta,weights, x,y,z):
-    result=x*weights[0]+y*weights[1]+z*weights[2]+weights[3]
-    if (result>theta):
-    	return 1
-    else:
-    	return 0 
-    
+'''
+End of visualization and start of the source code part  
 
-THETA=0.1
+'''
+
+THETA=0
 LEARNING_RATE=0.5
 MAX_ITER=7
+
+def getOutput(theta,weights, x,y,z):
+    result=x*weights[0]+y*weights[1]+z*weights[2]+weights[3]
+    if (result<=THETA):
+    	return 0
+    else:
+    	return 1 
 
 X=[0,0,0,1,7,7,7,7]
 Y=[0,1,2,3,0,1,2,3]
@@ -41,22 +50,21 @@ Z=[0,0,0,0,0,0,0,0]
 
 
 weights=[0,0,0,0] 
-outputs=[1,1,1,1,-1,-1,-1,-1]
-globalError=10
+expected=[1,1,1,1,-1,-1,-1,-1]
 iteration=0
 
 while iteration<=MAX_ITER:
     for i in range(len(X)-1):
-        output=calculateOutput(THETA,weights,float(X[i]),float(Y[i]),float(Z[i]))
-        Error=float(outputs[i])-output
-        weights[0]=float(weights[0])+LEARNING_RATE*Error*X[i]
-        weights[1]=float(weights[1])+LEARNING_RATE*Error*Y[i]
-        weights[2]=float(weights[2])+LEARNING_RATE*Error*Z[i]
-        weights[3]=float(weights[2])+LEARNING_RATE*Error
+        observed=getOutput(THETA,weights,float(X[i]),float(Y[i]),float(Z[i]))
+        error=float(expected[i])-observed
+        weights[0]=float(weights[0])+LEARNING_RATE*error*X[i]
+        weights[1]=float(weights[1])+LEARNING_RATE*error*Y[i]
+        weights[2]=float(weights[2])+LEARNING_RATE*error*Z[i]
+        weights[3]=float(weights[2])+LEARNING_RATE*error
         
     iteration=iteration+1
 
 print(weights)    
 
-print(calculateOutput(THETA,weights,0,4,0))
-print(calculateOutput(THETA, weights,7,3.5,0))
+print(getOutput(THETA,weights,0,4,0))
+print(getOutput(THETA, weights,7,3.5,0))
