d1a8f018783c7905e3a4bb9e515670aca89e86af
shivapbhusal <civabhusal@gmail.com>
2018-02-20 23:23:28 -0500

    add 3 class feature

--- perceptron.cpp
+++ perceptron.cpp
@@ -31,23 +31,32 @@ int trainingSetC[4][4]= {
   { 1,1,1,1 }
 };
 
+
 Perceptron P1(0.5,0); 
 P1.trainModel(trainingSetA); 
 
 Perceptron P2(0.5,0); 
 P2.trainModel(trainingSetB); 
 
+Perceptron P3(0.5,0); 
+P3.trainModel(trainingSetC); 
+
 for (int i=0;i<=3;i++)
 {
    double  resultA=P1.getFinalResult(trainingSetA[0][i],trainingSetA[1][i],trainingSetA[2][i]);
    double  resultB=P2.getFinalResult(trainingSetA[0][i],trainingSetA[1][i],trainingSetA[2][i]);
-
-   if (resultA>=resultB)
+   double resultC=P3.getFinalResult(trainingSetA[0][i],trainingSetA[1][i],trainingSetA[2][i]);
+   
+   if (resultA>resultB and resultA>resultC)
    {
    	std::cout<<"Class A"; 
    }
+   else if (resultB>resultA and resultB>resultC)
+   {
+    std::cout<<"Class B"; 
+   }
    else {
-   	std::cout<<"Class B"; 
+   	std::cout<<"Class C"; 
    }
 
 }
