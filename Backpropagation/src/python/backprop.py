36c7bd1ba27fe89f866bba96647a2a7fecff98d8
shivapbhusal <civabhusal@gmail.com>
2018-03-07 17:11:11 -0500

    add further

--- Backpropagation/src/python/backprop.py
+++ Backpropagation/src/python/backprop.py
@@ -3,12 +3,28 @@ Python Implementation of BackPropagation Algorithm
 Author: Shiva Bhusal, BGSU 
 
 '''
+import math as math
 
 class Node():
+    '''
+    A Node class
+    Has Input and the corresponding output function. 
+    '''
+    def __init(self):
+        self.output=0
 
     def calculateOutput(self,inputValue):
-        return inputValue
+        self.output=(1/(1+math.pow(math.e,inputValue)))
 
-node=Node()
-print(node.calculateOutput(10))
+    def getOutput(self):
+        return self.output
 
+inputs=[1,2]
+outputs=[1]
+
+weights=[0.5,0.4,0.1,0,0.9,0.1]
+N1=Node()
+N1.calculateOutput(10)
+N2=Node()
+N3=Node()
+print(N1.output)
