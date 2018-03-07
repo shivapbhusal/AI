c7e5779fd47ba3b8c50236af84804d94bfbc8698
shivapbhusal <civabhusal@gmail.com>
2018-03-07 16:33:24 -0500

    add Python draft

---
+++ Backpropagation/src/python/backprop.py
@@ -0,0 +1,14 @@
+'''
+Python Implementation of BackPropagation Algorithm 
+Author: Shiva Bhusal, BGSU 
+
+'''
+
+class Node():
+
+    def calculateOutput(self,inputValue):
+        return inputValue
+
+node=Node()
+print(node.calculateOutput(10))
+
