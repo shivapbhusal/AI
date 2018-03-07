911e29e304a7e387d4d7000fd6b682e5b8cd7b9a
shivapbhusal <civabhusal@gmail.com>
2018-03-07 18:29:20 -0500

    get first output

--- Backpropagation/src/python/backprop.py
+++ Backpropagation/src/python/backprop.py
@@ -29,21 +29,34 @@ class Node():
 
 inputs=[1,2]
 outputs=[1]
+b1=0.5 
+b2=0.5
 
 '''
 We assume, we have two inputs, a hidden layer with two nodes, and a output Node. 
 So, hidden layer will have 4 weights whereas output layer will have two weights. 
 
 '''
-weightList=[[0.5,0.2,0.8,0.1],[0.5,0.5]]
+W=[[0.5,0.2,0.8,0.1],[0.5,0.5]]
 N1=Node()
 N1.calculateOutput(10)
 N2=Node()
 N3=Node()
 
-for layers in weightList:
-    for weights in layers:
-        print(weights)
+# Step 1
+
+inputN1=W[0][0]*inputs[0]+W[0][1]*inputs[1]
+N1.calculateOutput(inputN1)
+
+inputN2=W[0][2]*inputs[0]+W[0][3]*inputs[1]
+N2.calculateOutput(inputN2)
+
+inputN3=N1.getOutput()*W[1][0]+N2.getOutput()*W[1][1]
+N3.calculateOutput(inputN3)
+
+# Check the output of Step 1
+print(N3.getOutput())
+        
 
 
 
