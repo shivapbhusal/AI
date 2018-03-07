0cddc85e7498b5c26787f5b7e45947714175f785
shivapbhusal <civabhusal@gmail.com>
2018-03-07 17:42:46 -0500

    modify further

--- Backpropagation/src/python/backprop.py
+++ Backpropagation/src/python/backprop.py
@@ -1,7 +1,6 @@
 '''
 Python Implementation of BackPropagation Algorithm 
 Author: Shiva Bhusal, BGSU 
-
 '''
 import math as math
 
@@ -12,6 +11,8 @@ class Node():
     '''
     def __init(self):
         self.output=0
+        self.w1=0
+        self.w2=0
 
     def calculateOutput(self,inputValue):
         self.output=(1/(1+math.pow(math.e,inputValue)))
@@ -19,12 +20,30 @@ class Node():
     def getOutput(self):
         return self.output
 
+    def updateW1(self,newValue):
+        self.w1=newValue
+    
+    def updateW2(self,newValue):
+        self.W2=newValue 
+
+
 inputs=[1,2]
 outputs=[1]
 
-weights=[0.5,0.4,0.1,0,0.9,0.1]
+'''
+We assume, we have two inputs, a hidden layer with two nodes, and a output Node. 
+So, hidden layer will have 4 weights whereas output layer will have two weights. 
+
+'''
+weightList=[[0.5,0.2,0.8,0.1],[0.5,0.5]]
 N1=Node()
 N1.calculateOutput(10)
 N2=Node()
 N3=Node()
-print(N1.output)
+
+for layers in weightList:
+    for weights in layers:
+        print(weights)
+
+
+
