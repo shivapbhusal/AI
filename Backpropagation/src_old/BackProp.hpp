96ea6b7a04584c556151a274e18f1d9596879412
shivapbhusal <civabhusal@gmail.com>
2018-03-18 18:46:34 -0400

    add files, one class is wrongly classified

--- Backpropagation/src_old/BackProp.hpp
+++ Backpropagation/src_old/BackProp.hpp
@@ -14,26 +14,25 @@ class Node
 public:
 	double const THETA=0.5; 
 	double w1=0.1; 
-	double w2=0.1; 
+	double w2=0.5; 
 	double w3=0.1; 
 	double bias=0.1; 
 	double total=0; 
 	double output=0;
 
-	void updateWeight(double adjustment, double indicator)
+	Node(double wght1, double wght2, double wght3)
 	{
-		if (indicator==1){
-		w1=w1-THETA*adjustment; 	
-		}
-		else if (indicator==2){
-			w2=w2-THETA*adjustment; 
-		}
-		else if (indicator==3){
-			w3=w3-THETA*adjustment; 
-		}
-		else {
-			std::cout<<"Invalid value of indicator"; 
-		}
+		w1=wght1; 
+		w2=wght2; 
+		w3=wght3; 
+	}
+
+	void updateWeight(double adj1, double adj2, double adj3)
+	{
+		
+		w1=w1-THETA*adj1; 	
+		w2=w2-THETA*adj2; 
+		w3=w3-THETA*adj3; 
 	}
 
 	double getOutput(double x, double y, double z)
@@ -57,20 +56,20 @@ class BackProp{
 
 		double errorTotal=1;
 		// Create three hidden nodes. 
-		Node *H1=new Node(); 
-		Node *H2=new Node(); 
-		Node *H3=new Node(); 
+		Node *H1=new Node(0.5, 0.9,0.1); 
+		Node *H2=new Node(0.2, 0.1, 0.6); 
+		Node *H3=new Node(0.7,0.8,0.9); 
 
 		//Create three output Nodes. 
-	    Node *out1=new Node(); 
-	    Node *out2=new Node(); 
-	    Node *out3=new Node(); 
+	    Node *out1=new Node(0.1,0.3,0.9); 
+	    Node *out2=new Node(0.9,0.4,0.4); 
+	    Node *out3=new Node(0.2,0.25,0.30); 
 
 
 	void trainModel(double trainingData[3][45])
 	{
 		int count=0; 
-		while (count<50000)
+		while (count<500000)
 		{
 			count=count+1; 
 		double expectedA=0, expectedB=0,expectedC=0; // Initialize  
@@ -78,27 +77,28 @@ class BackProp{
 		{
 			if (i<15)
 			{
-				expectedA=0.8; 
-				expectedB=0.1; 
-				expectedC=0.1; 
+				expectedA=1; 
+				expectedB=0; 
+				expectedC=0; 
 			}
 
 			else if ((i>=15) && (i<30))
 			{
-				expectedA=0.1; 
-				expectedB=0.8; 
-				expectedC=0.1; 
+				expectedA=-1; 
+				expectedB=-1; 
+				expectedC=-1; 
 
 			}
 			else if ((i>=30) && (i<45)) 
 			{
-				expectedA=0.1; 
-				expectedB=0.1; 
-				expectedC=0.8; 
+				expectedA=1; 
+				expectedB=1; 
+				expectedC=1; 
 			}
 			else {
 				std::cout<<"Extra training dataSets detected."; 
 			}
+
 			double outNetH1=H1->getOutput(trainingData[0][i],trainingData[1][i],trainingData[2][i]); 
 			double outNetH2=H2->getOutput(trainingData[0][i],trainingData[1][i],trainingData[2][i]); 
 			double outNetH3=H3->getOutput(trainingData[0][i],trainingData[1][i],trainingData[2][i]); 
@@ -115,37 +115,24 @@ class BackProp{
 
 			errorTotal=errorA+errorB+errorC; 
 
-			double adjustment=-(expectedA-resultA)*(out1->output)*(1-(out1->output))*H1->output;  
-			out1->updateWeight(adjustment,1); 
-			out1->updateWeight(adjustment,2);
-			out1->updateWeight(adjustment,3); 
+			double adj01=-(expectedA-resultA)*(out1->output)*(1-(out1->output))*H1->output; 
+			double adj02=-(expectedA-resultA)*(out1->output)*(1-(out1->output))*H2->output;  
+			double adj03=-(expectedA-resultA)*(out1->output)*(1-(out1->output))*H3->output;  
+			out1->updateWeight(adj01,adj02,adj03);  
 
-			adjustment=-(expectedA-resultA)*(out2->output)*(1-(out2->output))*H2->output;  
-			out2->updateWeight(adjustment,1); 
-			out2->updateWeight(adjustment,2);
-			out2->updateWeight(adjustment,3); 
+			double adj11=-(expectedA-resultA)*(out2->output)*(1-(out2->output))*H1->output; 
+			double adj12=-(expectedA-resultA)*(out2->output)*(1-(out2->output))*H2->output;  
+			double adj13=-(expectedA-resultA)*(out2->output)*(1-(out2->output))*H3->output;  
+			out2->updateWeight(adj11,adj12,adj13);  
 
-			adjustment=-(expectedA-resultA)*(out3->output)*(1-(out3->output))*H3->output;  
-			out3->updateWeight(adjustment,1); 
-			out3->updateWeight(adjustment,2);
-			out3->updateWeight(adjustment,3); 
+			double adj21=-(expectedA-resultA)*(out3->output)*(1-(out3->output))*H1->output; 
+			double adj22=-(expectedA-resultA)*(out3->output)*(1-(out3->output))*H2->output;  
+			double adj23=-(expectedA-resultA)*(out3->output)*(1-(out3->output))*H3->output;  
+			out3->updateWeight(adj21,adj22,adj23);  
 
-			// Weight adjustment -- Two
-
-			adjustment=(H1->w1)*-(expectedA-resultA)*(out1->output)*(1-(out1->output));  
-			H1->updateWeight(adjustment,1); 
-			H1->updateWeight(adjustment,2);
-			H1->updateWeight(adjustment,3); 
-
-			adjustment=(H2->w1)*-(expectedA-resultA)*(out2->output)*(1-(out2->output));  
-			H2->updateWeight(adjustment,1); 
-			H2->updateWeight(adjustment,2);
-			H2->updateWeight(adjustment,3); 
-
-			adjustment=(H2->w1)*-(expectedA-resultA)*(out3->output)*(1-(out3->output));  
-			H3->updateWeight(adjustment,1); 
-			H3->updateWeight(adjustment,2);
-			H3->updateWeight(adjustment,3); 
+			H1->updateWeight(adj01*trainingData[0][i],adj11*trainingData[1][i], adj21*trainingData[2][i]); 
+			H2->updateWeight(adj02*trainingData[0][i],adj12*trainingData[1][i], adj22*trainingData[2][i]); 
+			H3->updateWeight(adj03*trainingData[0][i],adj13*trainingData[1][i], adj23*trainingData[2][i]); 
 
 		}
 		std::cout<<errorTotal<<" "<<count<<std::endl; 
@@ -155,22 +142,22 @@ class BackProp{
    void calculateFinal(double x, double y, double z)
    {
    	    double total1=H1->w1*x+H1->w2*y+H1->w3*z+H1->bias; 
-		double output1=1/(1+pow(2.7,total1));
+		double output1=tanh(total1); 
 
    	    double total2=H2->w1*x+H2->w2*y+H2->w3*z+H1->bias; 
-		double output2=1/(1+pow(2.7,total2));
+		double output2=tanh(total2);
 
 		double total3=H2->w1*x+H2->w2*y+H2->w3*z+H1->bias; 
-		double output3=1/(1+pow(2.7,total3));
+		double output3=tanh(total3);
 
 		double total4=out1->w1*output1+out1->w2*output2+out1->w3*output3+out1->bias;
-		finalOutput1=1/(1+pow(2.7,total4));
+		finalOutput1=tanh(total4);
 
 		double total5=out2->w1*output1+out2->w2*output2+out2->w3*output3+out2->bias;
-		finalOutput2=1/(1+pow(2.7,total5));
+		finalOutput2=tanh(total5);
 
 		double total6=out3->w1*output1+out3->w2*output2+out3->w3*output3+out3->bias;
-		finalOutput3=1/(1+pow(2.7,total6));
+		finalOutput3=tanh(total6);
 
 		std::cout<<finalOutput1<<" "<<finalOutput2<<" "<<finalOutput3<<std::endl; 
    }
--- Backpropagation/src_old/main.cpp
+++ Backpropagation/src_old/main.cpp
@@ -13,7 +13,7 @@ int  main()
 
 std::cout<<"Centers of spheres:"<<"(0,50,-50),(-50,-50,-50),(50,50,50)"<<std::endl<<std::endl; 
 
-Generate A(0,50,-50); 
+Generate A(50,50,-50); 
 A.setDataValues(); // generates 30 datasets around the chosen center. 
 A.readDataValues(); 
 
@@ -54,6 +54,16 @@ network.trainModel(finalTrainingSet);
 for (int i=15;i<30;i++)
 {
   network.calculateFinal(A.trainingSet[1][i],A.trainingSet[2][i],A.trainingSet[2][i]); 
+}
+
+for (int i=15;i<30;i++)
+{
+  network.calculateFinal(B.trainingSet[1][i],B.trainingSet[2][i],B.trainingSet[2][i]); 
+}
+
+for (int i=15;i<30;i++)
+{
+  network.calculateFinal(C.trainingSet[1][i],C.trainingSet[2][i],C.trainingSet[2][i]); 
 }
 
 }
