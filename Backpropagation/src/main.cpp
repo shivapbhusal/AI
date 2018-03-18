c6a76177a603dfbb0bbf721d8ce09391340bee11
shivapbhusal <civabhusal@gmail.com>
2018-03-18 09:00:43 -0400

    add BackPropNet initial draft

--- Backpropagation/src/main.cpp
+++ Backpropagation/src/main.cpp
@@ -6,8 +6,7 @@ Author: Shiva Bhusal, CS 6200
 
 #include<iostream>
 #include "generateData.hpp"
-#include "BackProp.hpp"
-#include "Classify.hpp"
+#include "BackPropNet.hpp"
 
 int  main()
 {
@@ -26,12 +25,13 @@ Generate C(50,50,50);
 C.setDataValues(); 
 C.readDataValues(); 
 
-// Initial setup for Organized form of training dataset 
-double finalTrainingSet[3][45]=
+double finalTrainingSet[4][45]=
 {
   { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
   { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-  { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+  { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  {  0.9,0.9,0.9,0.9,0.9,0.9,0.9,0.9,0.9,0.9,0.9,0.9,0.9,0.9,0.9, 0.5,0.5,0.5,0.5,0.5,0.5,0.5,
+    0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5, 0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1}
 };
 
 // Get the training DataSet in organized form 
@@ -49,12 +49,5 @@ for (int i=0;i<15;i++)
    finalTrainingSet[2][i+30]=C.trainingSet[2][i]; 
 }
 
-BackProp network; 
-network.trainModel(finalTrainingSet);  
-
-for (int i=15;i<30;i++)
-{
-  network.calculateFinal(A.trainingSet[1][i],A.trainingSet[2][i],A.trainingSet[2][i]); 
-}
 
 }
