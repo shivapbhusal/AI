bd9c49e09460c50cdac4fc9e878444224a39e06d
shivapbhusal <civabhusal@gmail.com>
2018-03-17 21:00:15 -0400

    add draft

--- Backpropagation/src/nn.cpp
+++ Backpropagation/src/nn.cpp
@@ -5,17 +5,18 @@ Based on a Video Tutorial
 */ 
 
 #include<vector>
+#include<iostream>
 
-class Neuron; 
+class Neuron{}; 
 typedef std::vector<Neuron> Layer;
 
 class Net
 {
 public:
 	Net(const std::vector<unsigned> &topology); 
-	void feedForward(const std::std::vector<double> &inputVals); 
-	void backProp(const std::vector<double> &targetVals); 
-	void getResults(std::vector<double> &resultVals) const; 
+	void feedForward(const std::vector<double> &inputVals){}; 
+	void backProp(const std::vector<double> &targetVals){}; 
+	void getResults(std::vector<double> &resultVals) const {}; 
 
 private: 
 	std::vector<Layer> m_layers;
@@ -24,12 +25,25 @@ private:
 Net::Net(const std::vector<unsigned> &topology)
 {
 	unsigned numLayers=topology.size(); 
+	for (unsigned layerNum=0; layerNum<=numLayers; ++layerNum){
+		m_layers.push_back(Layer()); 
+
+		// We have got a new layer and now we need an inner loop to add nodes to this layer. 
+
+		for (unsigned neuronNum=0; neuronNum<=topology[layerNum]; ++neuronNum){
+			m_layers.back().push_back(Neuron()); 
+			std::cout<<"Made a Neuron !"<<std::endl; 
+		}
+	}
 }
 
 int main()
 {
 	// Eg. 3,2,1 
 	std::vector<unsigned> topology;
+	topology.push_back(3); 
+	topology.push_back(2); 
+	topology.push_back(1); 
 	Net myNet(topology); 
 
 	std::vector<double> inputVals;
