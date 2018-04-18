fddeb6c9bdfaa56f87bc3dd311115a6fdf745267
shivapbhusal <civabhusal@gmail.com>
2018-04-18 11:26:00 -0400

    add LinkedList

---
+++ clustering/KMeans.hpp
@@ -0,0 +1,24 @@
+/*
+KMeans clustering implementation in C++ 
+CS 6200
+Shiva Bhusal, Bowling Green State University
+*/
+
+#ifndef KMEANS_H 
+#define KMEANS_H
+#include<iostream> 
+
+class KMeans {
+private:
+	int k=0; 
+	
+public:
+	KMeans(int valueOfK){
+		k=valueOfK; 
+	}
+
+	
+
+}; 
+
+#endif 
---
+++ clustering/main.cpp
@@ -0,0 +1,10 @@
+/*
+Main file for cpp implementation of KMeans Clustering. 
+*/
+
+#include<iostream>
+#include "KMeans.hpp"
+int  main()
+{
+std::cout<<"Hello!, K means clustering"<<std::endl<<std::endl; 
+}
